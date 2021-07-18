f7db6188f5c45d7ca1959e4c793b5d9d6b26b905
joserodriguez <jars97@gmail.com>
2021-07-18 16:00:39 -0400

    wip

--- src/main/java/com/example/arquetype/MainApp.java
+++ src/main/java/com/example/arquetype/MainApp.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MainApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MainApp.class, args);
 	}
 	
 	@Bean
